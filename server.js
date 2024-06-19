5a3b4d5491ad1798d2a8d31233b68e107c3c5dfb
JoglTech <jakob21.hinterleitner@gmail.com>
2024-06-19 19:13:25 +0200

    static server is running without database

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+//const SQLiteMemberMangager = require("./scripts/SQLiteManager");
+
+
+const PORT = 8000;
+const HOST = "localhost";
+
+const app = express();
+app.use(express.json());
+app.use(express.static('static'));
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//const sqliteManager = new SQLiteMemberMangager();
+
+app.get('/', (req, res) => {
+  const pathToFile = path.join(__dirname, 'static', 'html', 'index.html');
+  res.sendFile(pathToFile);
+});
+
+/*
+app.post('/api/members', async (req, res) => {
+    const member = req.body;
+    const id = await sqliteManager.addMember(member);
+    member.href = `/api/members/${id}`;
+    res
+      .status(201)
+      .location(`/api/members/${id}`)
+      .send(member);
+  });
+*/
+const server = app.listen(PORT, () => {
+    console.log(`Webservice läuft unter http://${HOST}:${PORT}`);
+});
---
+++ static/scripts/SQLiteManager.js
@@ -0,0 +1,36 @@
+const sqlite3 = require("sqlite3");
+
+const db = new sqlite3.Database("rodelrennen.db");
+
+module.exports = class SQLiteMemberMangager {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS 
+                members (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    firstName TEXT, 
+                    lastName TEXT, 
+                    birthyear INTEGER,
+                    gender TEXT,
+                )`
+        );
+    }
+
+    async addMember(member) {
+        return new Promise((resolve, reject) => {
+            db.run(
+              `INSERT
+                INTO members (firstName, lastName, birthyear, gender)
+                VALUES (?, ?, ?, ?, ?)`,
+              [member.firstName, member.lastName, member.birthyear, member.gender],
+              function() {
+                resolve(this.lastID);
+              }
+            );
+        });
+    }
+}
