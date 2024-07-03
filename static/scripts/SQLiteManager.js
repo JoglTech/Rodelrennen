744f5ba9317b3da82664dba72e9aa172943446bd
JoglTech <jakob21.hinterleitner@gmail.com>
2024-07-03 20:51:40 +0200

    Added new functions to SQLiteManager

--- static/scripts/SQLiteManager.js
+++ static/scripts/SQLiteManager.js
@@ -37,4 +37,61 @@ module.exports = class SQLiteMemberMangager {
             );
         });
     }
-}
+
+    async getMember(id) {
+        return new Promise((resolve, reject) => {
+            db.get("SELECT * FROM members WHERE id = ?", [id], (error, row) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    async updateMember(id, member) {
+        return new Promise((resolve, reject) => {
+            db.run(
+                `UPDATE members SET 
+                firstName = ?, 
+                lastName = ?, 
+                birthyear = ?
+                gender = ? 
+                WHERE id = ?`,
+                [member.fname, member.sname, member.birthyear, member.gender, id],
+                (error, row) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(row);
+                    }
+                }
+            );
+        });
+    }
+
+    async deleteMember(id) {
+        return new Promise((resolve, reject) => {
+            db.run("DELETE FROM members WHERE id = ?", [id], (error, row) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    async getMembers() {
+        return new Promise((resolve, reject) => {
+            db.all("SELECT * FROM members", [], (error, rows) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+};
