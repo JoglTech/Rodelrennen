96ac1c44dc0e16591ed9d734aba0f97cceee5ed0
JoglTech <jakob21.hinterleitner@gmail.com>
2024-12-31 14:28:30 +0100

    batch datei für windows um das programm auszuführen

---
+++ windows/Rodelrennen.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\path\to\your\project"  // Pfad zu deinem Projektordner
+node server.js                   // Startet den Node.js Server
+start http://localhost:8000      // Öffnet den Browser und ruft die URL auf
